717f4248f2d8b4aa5eaefe0b4a2fcf730ade0317
anshusingh001 <anshusinghak001@gmail.com>
2023-09-13 03:03:25 +0530

    all added

---
+++ script.js
@@ -0,0 +1,7 @@
+/*var typed = new Typed('.static-txt', {
+    strings:["Hello","get out","fine"],
+    typespeed:150,
+    backspeed:150,
+    loop:true
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+})*/
